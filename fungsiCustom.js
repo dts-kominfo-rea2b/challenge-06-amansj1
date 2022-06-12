c3b4e9aad787e42ffee03063181c7f068b961de3
amansj1 <51205338+amansj1@users.noreply.github.com>
2022-06-12 22:35:45 +0700

    duni word sekai

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require("fs");
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,51 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  getCurFile((firstWord) => {
+    getNextFile(firstWord, (secondWord) => {
+      getLastFile(secondWord, (arr) => {
+        fnCallback(null, arr);
+      });
+    })
+  });
+};
+
+function kataB(kata) {
+  var w = kata.split(" ");
+  return w[w.length - 1];
+}
+
+function getCurFile(fl){
+ fs.readFile(file1, (err,data) =>{
+   if (err) throw err
+   var data = JSON.parse(data);
+   var kata = [];
+   kata.push(kataB(data.message))
+   fl(kata);
+ });
+}
+
+function getNextFile(kata, fl){
+  fs.readFile(file2,(err, data)=> {
+    if (err) throw err
+    var data = JSON.parse(data);
+    kata.push(kataB(data[0].message));
+    fl(kata);
+  });
+}
+
+function getLastFile(kata, fl){
+  fs.readFile(file3, (err,data)=>{
+    if (err) throw err
+    var data = JSON.parse(data);
+    kata.push(kataB(data[0].data.message));
+    fl(kata);
+  });
+}
+
+
+
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
